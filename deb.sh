19868649876620e59f2c01b51b97dffd7804be09
djvu <djvu@inbox.ru>
2019-05-15 20:56:06 +0300

    Скрипты управления

--- deb.sh
+++ deb.sh
@@ -4,12 +4,18 @@ DEB=deb
 BUILD_DIR=.
 
 PATH_DEB=$DEB/usr/lib/systemd/system/
+PATH_CAR=$DEB/usr/local/car
 
 chmod 755 $DEB/*
 
 rm -r $PATH_DEB
 mkdir -p $PATH_DEB
 
+rm -r $PATH_CAR
+mkdir -p $PATH_CAR/bin/
+
 cp -r --parents car.service $PATH_DEB/
+
+cp -r --parents bin/*.sh $PATH_CAR/
 
 dpkg-deb --build $DEB $BUILD_DIR
